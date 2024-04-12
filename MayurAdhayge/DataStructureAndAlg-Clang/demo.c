c005771fe82b77823a98c3597bf1f389f825a3f9
centiago_001 <mayuradhayge@gmail.com>
2024-04-12 09:34:05 +0530

    exercise by mayur

---
+++ MayurAdhayge/DataStructureAndAlg-Clang/demo.c
@@ -0,0 +1,21 @@
+#include<sto.h>
+
+
+void func();
+static int count=5;
+int  main()
+{
+while(count--)
+{
+func();
+}
+return 0;
+}
+
+void func()
+{
+static int i=5;
+i++;
+printf("i is %d and count is%d\n,i,count");
+
+}
