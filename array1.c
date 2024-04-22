d909cef58ed56bbef12aae7fb41c2fe5953b8348
puttasusmitha <susmithaputta24@gmail.com>
2024-04-22 15:16:20 +0530

    susmitha

---
+++ array1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int * getRandom()
+{
+	static int r[10];
+	int i;
+	srand((unsigned)time( NULL ));
+	for( i=0; i<10; ++i)
+	{
+		r[i] = rand();
+		printf("r[%d]= %d\n", i, r[i]);
+				}
+				return r;
+				}
+
