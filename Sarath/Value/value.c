e7b093580775f73c92c4e160007c4f821b87dbec
Sarath <sarathchandramudiganti8@gmail.com>
2024-04-22 17:20:05 +0530

    adding code

---
+++ Sarath/Value/value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("Enter numbers:");
+	scanf("%d %d",&num1,&num2);
+	printf("NUM1 =%d NUm2=%d\n",num1,num2);
+	printf("Add is %d\n",num1+num2);
+	return 0;
+}
