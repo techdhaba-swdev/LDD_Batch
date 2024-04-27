c6a16bc4240e6af1dab813d7d179e3fbe5d6b84f
Sarath <sarathchandramudiganti8@gmail.com>
2024-04-27 17:16:51 +0530

    adding analysis and coverage

--- Sarath/ModularProgramming/queue_secure_modular/queuemain.c
+++ Sarath/ModularProgramming/queue_secure_modular/queuemain.c
@@ -3,17 +3,17 @@
 #include"queue.h"
 int main()
 {
-
-	enqueue(40);
-   	enqueue(30);
+	//Enqueuing element to the queue from first
+	enqueue(40); 
+   	enqueue(30); 
    	enqueue(20);
    	enqueue(10);
-        print();
-        int d1=dequeue();
-        printf("Dequeued element is %d\n",d1);
+        print(); //printing enqueued elements using print() API
+        int d1=dequeue(); //returned dequeued value from dequeue API and taken to another variable
+        printf("Dequeuing element is %d\n",d1); //Printing the dequeued element 
         print();
         int d2=dequeue();
-        printf("Dequeued element is %d\n",d2);
+        printf("Dequeuing element is %d\n",d2);
         print();
 
         return 0;
