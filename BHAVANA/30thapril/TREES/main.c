80f01abe536faf55fe8a9290607c874be044ebb1
bhavanaudupa12 <udupabhavana2001@gmail.com>
2024-04-30 17:56:58 +0530

    today's commit

--- BHAVANA/30thapril/TREES/main.c
+++ BHAVANA/30thapril/TREES/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include "binary_tree.h"
 
 int main() {
     struct Node* root = NULL;
+    clock_t start, end; // Variables to store start and end time
+     double cpu_time_used; //Variables to store CPU time used
+     
+     //Start clock
+     start = clock();
+
     // Insert nodes into the binary tree
     root = insert(root, 50);
     insert(root, 30);
@@ -17,10 +25,17 @@ int main() {
     inOrder(root);
     printf("\n");
 
+
     // Calculate the sum of all nodes in the binary tree
     int sum = add(root);
     printf("Sum of all nodes: %d\n", sum);
 
+    //stop clock
+    //end = clock();
+    
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("CPU time used: %f seconds\n", cpu_time_used);
     // Memory deallocation for the binary tree is not shown here to keep the example simple
 
     return 0;
