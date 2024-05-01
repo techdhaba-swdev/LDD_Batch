f3d1d3c383eef0f43a9ad420cde81f2ddb439d1f
birrubadri12 <birrubadrinath@gmail.com>
2024-05-01 10:13:48 +0530

    binarytree

--- badri/datastruct/binarytree/main.c
+++ badri/datastruct/binarytree/main.c
@@ -4,8 +4,10 @@
 #include "order.h"// includes order.h header file 
 #include "binary_tree.h"//includes binary_tree.h headerfile
 #include"sum.h"//includes sum.h headerfile
+#include<time.h>//includes time functions
 int main()
 {
+	clock_t start = clock();
 	struct Node *root = NULL;//assining NULL value to root
 	root = insert(root,50);//inserting 50 value to root 
 	insert(root,30);//inserting 30 value to root
@@ -19,7 +21,9 @@ int main()
 	printf("\n");//prints nest line
 	int sum = sumNodes(root);//assining return sumofnodes value to sum
 	printf("sum of root nodes %d\n",sum);//printing sum of all nodes
-
+	clock_t end = clock();
+	double time_taken = ((double)(start - end)) /CLOCKS_PER_SEC;
+	printf("time taken : %f\n",time_taken);
 	return 0;//returning value 0 successful termination of program
 	}
 
