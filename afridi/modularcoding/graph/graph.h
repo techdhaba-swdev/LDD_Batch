31f25a6f68a0597ea8c12c08020793964cfd503b
shaikh_afridi <shaikafridi79@gmail.com>
2024-05-02 22:26:44 +0530

    graph code makefile

---
+++ afridi/modularcoding/graph/graph.h
@@ -0,0 +1,10 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#define MAX_VERTICES 100
+
+void initializeGraph(int numVertices);
+void addEdge(int src, int dest);
+void printGraph(int numVertices);
+
+#endif /* GRAPH_H */
---
+++ afridi/modularcoding/graph/graphmain.c
@@ -0,0 +1,15 @@
+#include "graph.h"
+
+int main() {
+    int numVertices = 4;
+
+    initializeGraph(numVertices);
+
+    addEdge(0, 1);
+    addEdge(1, 2);
+    addEdge(2, 3);
+
+    printGraph(numVertices);
+
+    return 0;
+}
