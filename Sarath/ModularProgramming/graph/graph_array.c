e0d8718f42b6008ba2869f41a0b862138f0b1da7
Sarath <sarathchandramudiganti8@gmail.com>
2024-05-02 22:59:46 +0530

    modified graph

--- Sarath/ModularProgramming/graph/graph_array.c
+++ Sarath/ModularProgramming/graph/graph_array.c
@@ -3,17 +3,9 @@ int main()
 {
   int numVertices = 4;
   initializeGraph(numVertices);
-  //addEdge(0,0);
-  //addEdge(0, 1); // Directed edge from 0 to 1
-  //addEdge(1, 2); // Directed edge from 1 to 2
-  //addEdge(2, 3); // Directed edge from 2 to 3
-  for (int i = 0; i < numVertices; i++)
-  {
-    for (int j = 0; j < numVertices; j++)
-    {
-      addEdge(i,j);
-    }
-  }
+  addEdge(0, 1); // Directed edge from 0 to 1
+  addEdge(1, 2); // Directed edge from 1 to 2
+  addEdge(2, 3); // Directed edge from 2 to 3
   printGraph(numVertices);
   return 0;
 }
