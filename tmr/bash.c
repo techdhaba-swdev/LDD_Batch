39e1738a94bc5dffce1fe371dc4bd12d05972b76
RPS <Tirumal>
2024-05-14 22:43:54 +0530

    shell

---
+++ tmr/bash.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a = -98746;
+	char *b = "Tirumal";
+
+	if(a > 0)
+	{
+		printf("integer is +ve:\n ");
+	}
+	else if(a < 0)
+	{
+		printf("integer is -ve:\n ");
+	}
+	else 
+	{
+		printf("integer is null\n ");
+	}
+	return 0;
+
+
+}
+
---
+++ tmr/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Declare variables
+my_string="Tirumal!"
+my_integer=50
+
+# Check the value of the integer
+if [ $my_integer -gt 45 ]; then
+    echo "The integer is greater than 45."
+else
+    echo "The integer is less than or equal to 45."
+fi
