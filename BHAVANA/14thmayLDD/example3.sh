7b1cda8c75768be53a0ceed0fd0a45008f89ba19
bhavanaudupa12 <udupabhavana2001@gmail.com>
2024-05-14 17:27:49 +0530

    SHELL SCRIPT

---
+++ BHAVANA/14thmayLDD/example3.sh
@@ -0,0 +1,11 @@
+# Check if the script received at least two arguments
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <filename> <destination>"
+  exit 1  # Exit with an error code if not enough arguments
+fi
+
+# Process the first two arguments
+filename="$1"
+destination="$2"
+
+# ... (rest of your script using $filename and $destination)
