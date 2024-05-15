43ad981ea74496ee12743c420f0e3427f082723f
Kiruthiga_V <kiruthiga20009@gmail.com>
2024-05-15 09:14:43 +0530

    day29_files

---
+++ Kiruthiga/may14/shell_script/script1/script.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+# Declare variables
+my_string="Hello, world!"
+my_integer=10
+
+# If-else statement
+if [ $my_integer -gt 5 ]; then
+    echo "$my_string Integer is greater than 5."
+else
+    echo "$my_string Integer is not greater than 5."
+fi
