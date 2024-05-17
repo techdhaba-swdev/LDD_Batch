53a6a8782ac51b3ec4d9989ecda818af4532d491
VenkataSaichebrolu3 <chebroluvenkat3@gmail.com>
2024-05-17 20:26:04 +0530

    exam

--- Venkat/may17_exam/pthread/pthread.c
+++ Venkat/may17_exam/pthread/pthread.c
@@ -6,85 +6,85 @@
 #define ODD_FILE "odd_lines.txt"
 
 pthread_mutex_t lock;
-FILE *sourceFile,*evenFile,*oddFile;
+FILE *sourceFile, *evenFile, *oddFile;
 
 void *writeEven(void *arg) {
-	int lineNum = 0;
-	char buffer[256];
-	pthread_mutex_lock(&lock);
-	while (fgets(buffer,sizeof(buffer),sourceFile) != NULL)
-	{
-		lineNum++;
-		if (lineNum % 2 == 0) {
-			fputs(buffer,evenFile);
-		}
-	}
-	pthread_mutex_unlock(&lock);
-	return NULL;
-}
-void *writeOdd(void *arg) {
-        int lineNum = 0;
-        char buffer[256];
-	rewind(sourceFile);
-        pthread_mutex_lock(&lock);
-        while (fgets(buffer,sizeof(buffer),sourceFile) != NULL)
-        {
-                lineNum++;
-                if (lineNum % 2 != 0) {
-                        fputs(buffer,oddFile);
-                }
+    int lineNum = 0;
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), sourceFile) != NULL) {
+        lineNum++;
+        if (lineNum % 2 == 0) {
+            pthread_mutex_lock(&lock);
+            fputs(buffer, evenFile);
+            pthread_mutex_unlock(&lock);
         }
-        pthread_mutex_unlock(&lock);
-        return NULL;
+    }
+    return NULL;
 }
+
+void *writeOdd(void *arg) {
+    int lineNum = 0;
+    char buffer[256];
+    rewind(sourceFile);
+    while (fgets(buffer, sizeof(buffer), sourceFile) != NULL) {
+        lineNum++;
+        if (lineNum % 2 != 0) {
+            pthread_mutex_lock(&lock);
+            fputs(buffer, oddFile);
+            pthread_mutex_unlock(&lock);
+        }
+    }
+    return NULL;
+}
+
 void *readAndPrint(void *arg) {
-	FILE *file = (FILE *)arg;
-	char buffer[256];
-	while (fgets(buffer,sizeof(buffer),file) != NULL) {
-		printf("%s",buffer);
-	} return NULL;
+    FILE *file = (FILE *)arg;
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("%s", buffer);
+    }
+    return NULL;
 }
+
 int main() {
-	pthread_t threads[4];
+    pthread_t threads[4];
 
-	if(pthread_mutex_init(&lock,NULL) != 0) {
-		printf("mutex has falies\n");
-		return 1;
-	}
-	sourceFile = fopen("source.txt", "r");
-	evenFile = fopen(EVEN_FILE,"w");
-	oddFile = fopen(ODD_FILE,"w");
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("Mutex initialization has failed\n");
+        return 1;
+    }
 
-	if (sourceFile == NULL || evenFile == NULL || oddFile == NULL) {
-		printf("ERROR OPENING FILES");
-		return 1;
-	}
-	pthread_create(&threads[0],NULL,writeEven,NULL);
-	pthread_create(&threads[1],NULL,writeOdd,NULL);
+    sourceFile = fopen("source.txt", "r");
+    evenFile = fopen(EVEN_FILE, "w");
+    oddFile = fopen(ODD_FILE, "w");
 
-	pthread_join(threads[0],NULL);
-	pthread_join(threads[1],NULL);
-	
-	fclose(evenFile);
-	fclose(oddFile);
+    if (sourceFile == NULL || evenFile == NULL || oddFile == NULL) {
+        printf("Error opening files\n");
+        return 1;
+    }
 
-	evenFile = fopen(EVEN_FILE,"r");
-	oddFile = fopen(ODD_FILE,"r");
+    pthread_create(&threads[0], NULL, writeEven, NULL);
+    pthread_create(&threads[1], NULL, writeOdd, NULL);
 
-	pthread_create(&threads[2],NULL,readAndPrint,(void *)evenFile);
-	pthread_create(&threads[3],NULL,readAndPrint,(void *)oddFile);
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
 
-	pthread_join(&threads[2], NULL);
-        pthread_join(&threads[3], NULL);
+    fclose(evenFile);
+    fclose(oddFile);
 
-	fclose(sourceFile);
-	fclose(evenFile);
-	fclose(oddFile);
+    evenFile = fopen(EVEN_FILE, "r");
+    oddFile = fopen(ODD_FILE, "r");
 
-	pthread_mutex_destroy(&lock);
-	return 0;
+    pthread_create(&threads[2], NULL, readAndPrint, (void *)evenFile);
+    pthread_create(&threads[3], NULL, readAndPrint, (void *)oddFile);
+
+    pthread_join(threads[2], NULL);
+    pthread_join(threads[3], NULL);
+
+    fclose(sourceFile);
+    fclose(evenFile);
+    fclose(oddFile);
+
+    pthread_mutex_destroy(&lock);
+    return 0;
 }
-
-
-
- 
