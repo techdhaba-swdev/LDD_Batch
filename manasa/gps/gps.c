b944514b6c9a0a23f38116a3261d45375fb1f8e0
saimanasa-suvvada <saimanasasuvvada@gmail.com>
2024-05-17 17:34:22 +0530

    M2

---
+++ manasa/gps/gps.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define MAX_LENGTH 200
+int main()
+{
+        FILE *file;
+
+    double latitude;
+    double longitude;
+    double speed;
+    double course;
+    double variation;
+    char status;
+
+    file = fopen("gpsdata.txt","r");
+
+    if(file == NULL)
+{
+        printf("Error opening the file.\n");
+        return 1;
+}
+while(fscanf(file, "%ld %ld %ld %ld %ld", latitude,longitude,speed,course,variation) != EOF)
+{
+        printf("latitude: %ld\n", latitude);
+        printf("longitude: %ld\n", longitude);
+        printf("speed: %ld\n", speed);
+        printf("course: %ld\n", course);
+        printf("variation: %ld\n", variation);
+        printf("status: %s\n", status);
+}
+      fclose(file);
+
+       return 0;
+}
