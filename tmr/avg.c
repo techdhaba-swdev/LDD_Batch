c9c6ff4ddda680e255ae35acaf8b68ce6207f993
RPS <Tirumal>
2024-05-23 12:44:33 +0530

    programs

---
+++ tmr/avg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+	{
+		int num1, num2;
+		printf("enter the number1\n");
+		scanf("%d",&num1);
+ 
+		printf("enter the number2\n");
+		scanf("%d",&num2);
+
+
+		printf("avrg is %f \n", (float) (num1+num2)/2);
+
+		return 0;
+	}
--- tmr/string.c
+++ tmr/string.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
-#include <string.h>
-struct Books
-{
-	char title[50];
-	char author[50];
-	char subject[100];
-	int book_id;
-};
 
-int main()
-{
-	struct Books Book1;
-	struct Books Book2;
-	strcpy( Book1.title, "C programming");
-	strcpy( Book1.author," Nuha Ali");
-	strcpy(Book1.subject, " C programing Tutorial");
-	Boo1.book_id = 6495407;
-        strcpy( Book2.title, "Telecom Billing");
-        strcpy( Book2.author," Zara  Ali");
-        strcpy(Book2.subject, " Telecom Billing Tutorial");
-        Book2.book_id  = 6495700; 
-		
+#define MAX_LENGTH 100
 
+int main() {
+
+FILE *file;
+
+char name[MAX_LENGTH];
+
+int id;
+
+char subject[MAX_LENGTH];
+
+
+
+// Open the file for reading
+
+ file = fopen("students.txt", "r");
+
+
+
+if (file == NULL)
+{
+       	printf("Error opening the file.\n");
+       	return 1;
+
+}
+
+// Read data from the file and print it
+
+while (fscanf(file, "%s %d %s", name, &id, subject) != EOF) {
+
+printf("Name: %s\n", name);
+
+printf("ID: %d\n", id);
+
+printf("Subject: %s\n", subject);
+
+}
+
+// Close the file fclose(file);
+
+return 0;
 }
