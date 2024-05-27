8c9438f1d191893eef4e848ab723a19fa5fc5063
puttasusmitha <susmithaputta24@gmail.com>
2024-05-27 09:51:27 +0530

    susmitha

--- susmitha_putta/example1.c
+++ susmitha_putta/example1.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
- 
-int main() {
-
-    int i;
-
-    int sum = 0;
- 
-    for (i = 0; i <= 10; i++) {
-
-        sum += i;
-
-    }
- 
-    printf("The sum is: %d\n", sum);
- 
-    return 0;
-
+#include <string.h>
+int main()
+{
+char *strings[128];
+char string[256];
+char *p1, *p2;
+int nstrings;
+int found;
+int i, j;
+nstrings = 0;
+while (fgets(string, 256, stdin)) {
+for (i = 0; i < nstrings; i++) {
+found = 1;
+for (p1 = string, p2 = strings[i]; *p1 && *p2; p1++, p2++) {
+if (*p1 > *p2) {
+found = 0;
+break;
+}
+}
+if (found) break;
+}
+for (j = nstrings; j > i; j--)
+strings[j] = strings[j-1];
+strings[i] = strdup(string);
+nstrings++;
+if (nstrings >= 128) break;
+}
+for (i = 0; i < nstrings; i++)
+fprintf(stdout, "%s", strings[i]);
+return 0;
 }
 
--- susmitha_putta/main.c
+++ susmitha_putta/main.c
@@ -1,30 +1,14 @@
-#include <stdio.h>
-#include "sus.h"
-#include <time.h>
-
-int main() {
-	//intialise an empty binary search tree represented by the root node
-    struct Node* root = NULL;
-    clock_t start = clock();
-    //insert node with various values
-   root = insert(root, 50);
-   root= insert(root, 30);
-   root= insert(root, 20);
-   root= insert(root, 40);
-   root=insert(root, 70);
-   root=insert(root, 60);
-   root= insert(root, 80);
-//print elements in sorted order using in order traversal
-    printf("In-order traversal: ");
-    inOrder(root);
-    printf("\n");
-
-    int sum = calculateSum(root);
-    printf("Sum of all node values: %d\n", sum);
-    clock_t stop = clock();//stop the clock
-    double time = ((double)stop-start);//cal the stop-start time
-    printf("Time = %f\n",time);
+#include<stdio.h>
+#include "main.h"
+int main()
+{
+	push(10);
+	push(20);
+	push(30);
 
 
-    return 0;
+	printf("popped value:%d\n",pop());
+	printf("popped value:%d\n",pop());
+	return 0;
 }
+
