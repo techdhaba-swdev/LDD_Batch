aac0bb049a528cdf8f7ec6d71c880bd0ca2ab228
Kedarnath Puyyad <kedarnathpuyyad1234@gmail.com>
2024-05-31 12:49:58 +0530

    sys progam

---
+++ kedarnath/c_demo_code/may28/test2/.driver3.mod.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/rps/Desktop/KEDARNATH_PUYYAD/LDD_Batch/kedarnath/c_demo_code/may28/test2/driver3.mod := printf '%s\n'   driver3.o | awk '!x[$$0]++ { print("/home/rps/Desktop/KEDARNATH_PUYYAD/LDD_Batch/kedarnath/c_demo_code/may28/test2/"$$0) }' > /home/rps/Desktop/KEDARNATH_PUYYAD/LDD_Batch/kedarnath/c_demo_code/may28/test2/driver3.mod
---
+++ kedarnath/c_demo_code/may28/test2/modules.order
@@ -0,0 +1,1 @@
+/home/rps/Desktop/KEDARNATH_PUYYAD/LDD_Batch/kedarnath/c_demo_code/may28/test2/driver3.o
