697938decb9e99194344c666229aea6d0af11f6a
birrubadri12 <birrubadrinath@gmail.com>
2024-06-10 13:20:23 +0530

    test

--- badri/test3/problem1/header.h
+++ badri/test3/problem1/header.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef HEADER_H //checks weather header file included previous
+#define HEADER_H//defines header file atleast one time
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,14 +23,14 @@ typedef struct ListNode {
     struct ListNode* next;
 } ListNode;
 
-BSTNode* createBSTNode(Student data);
-BSTNode* addStudent(BSTNode* root, Student data);
-BSTNode* searchStudent(BSTNode* root, int id);
-BSTNode* findMin(BSTNode* root);
-BSTNode* deleteStudent(BSTNode* root, int id);
-void bstToList(BSTNode* root, ListNode** head);
-ListNode* sortedMerge(ListNode* a, ListNode* b);
-void splitList(ListNode* source, ListNode** front, ListNode** back);
-void mergeSort(ListNode** headRef);
-void printList(ListNode* head);
-#endif
+BSTNode* createBSTNode(Student data);//function to create new binary search tree node
+BSTNode* addStudent(BSTNode* root, Student data);//function to add a student to bst
+BSTNode* searchStudent(BSTNode* root, int id);//fuction to search for a student by id in the bst
+BSTNode* findMin(BSTNode* root);//function to find the minimum node in the bst
+BSTNode* deleteStudent(BSTNode* root, int id);//function to delete a student by id from the bst
+void bstToList(BSTNode* root, ListNode** head);//function to convert the bst to a linked list
+ListNode* sortedMerge(ListNode* a, ListNode* b);//function to merge two sorted linked lists
+void splitList(ListNode* source, ListNode** front, ListNode** back);//function to split a linked list in to two halves
+void mergeSort(ListNode** headRef);//function to sort the linked list using merge sort
+void printList(ListNode* head);//function to print the linked list
+#endif//end of ifndef directive
